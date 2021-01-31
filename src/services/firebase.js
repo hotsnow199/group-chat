dc18581f7fe955dddaeb35806702a795a4c777d6
hotsnow199 <49254639+hotsnow199@users.noreply.github.com>
2021-01-31 14:10:21 +0700

    test-Databse

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,15 +4,15 @@ import 'firebase/database';
 
 // import './firebase';
 
-const config = {
-  apiKey: 'AIzaSyArbmBRG4-elnoN-TNMl8956TrSJmgNmrQ',
-  authDomain: 'react-chat-app-8d34c.firebaseapp.com',
-  projectId: 'react-chat-app-8d34c',
-  storageBucket: 'react-chat-app-8d34c.appspot.com',
-  messagingSenderId: '367673712417',
-  appId: '1:367673712417:web:7998277113b5057ef3a860',
-  measurementId: 'G-DJQ2XJ06Q5',
+const firebaseConfig = {
+  apiKey: 'AIzaSyDanCxyt1wKcz2Jv9-Yf41h1dDILn-Fsr4',
+  authDomain: 'test-database-a130d.firebaseapp.com',
+  databaseURL: 'https://test-database-a130d-default-rtdb.firebaseio.com',
+  projectId: 'test-database-a130d',
+  storageBucket: 'test-database-a130d.appspot.com',
+  messagingSenderId: '1014012499105',
+  appId: '1:1014012499105:web:5d2bf8b3c512b9d7c8cce0',
 };
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth;
 export const db = firebase.database();
